c79954d8cf92084ce9a0f57c579c34edd9fbecc9
shalom69 <108269015+shalom69@users.noreply.github.com>
2023-02-12 21:26:51 +0300

    1

--- src/test/java/ru/netology/servise/CashBackHackerTest.java
+++ src/test/java/ru/netology/servise/CashBackHackerTest.java
@@ -1,4 +1,21 @@
 import static org.junit.jupiter.api.Assertions.*;
 class CashBackHackerTest {
-  
+      @Test
+    void remain(int amount) {
+        CashBackHackerTest service = new CashBackHackerTest();
+        int expected = 0;
+        int amount = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void ShouldReturnHundred() {
+        CashBackHackerTest service = new CashBackHackerTest();
+        int expected = 100;
+        int amount = 900;
+        int actual = service.remain(amount);
+
+        assertEquals(expected,actual);
 }
