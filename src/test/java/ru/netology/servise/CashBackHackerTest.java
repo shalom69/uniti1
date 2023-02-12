387bc451faa478fb540ad168f2cc61e3f4afb078
shalom69 <108269015+shalom69@users.noreply.github.com>
2023-02-12 21:57:12 +0300

    Update CashBackHackerTest.java

--- src/test/java/ru/netology/servise/CashBackHackerTest.java
+++ src/test/java/ru/netology/servise/CashBackHackerTest.java
@@ -1,4 +1,28 @@
+package ru.netology.servise;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 public class CashBackHackerTest {
-  
+    @Test
+    public void ShouldReturnZero() {
+
+        CashBackHackerTest service = new CashBackHackerTest();
+        int expected = 0;
+        int amount = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+    
+
+    @Test
+    public void ShouldReturnHundred() {
+        CashBackHackerTest service = new CashBackHackerTest();
+        int expected = 100;
+        int amount = 900;
+        int actual = service.remain(amount);
+
+        assertEquals(expected,actual);
+    }
 }
